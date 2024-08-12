e69524bee8d117084f302f467087bd9f23e22ab7
Nuhu Sulemana <turosung@gmail.com>
2024-08-12 20:04:12 +0100

    Removed comments and old network code

--- TrendingMovies/Networking/APICaller.swift
+++ TrendingMovies/Networking/APICaller.swift
@@ -7,8 +7,6 @@
 
 import Foundation
 
-// Always remember to use enums to handle any network errors
-
 enum NetworkError: Error {
     case urlError
     case canNotParseData
@@ -38,41 +36,3 @@ public class APICaller {
         }
     }
 }
-
-
-
-
-
-
-
-
-//MARK: - ASYNC CLASS FOR NETWORK CALL TO FETCH DATA FROM MOVIEDB WEBSITE. WILL USE IT LATER
-//public class APICaller {
-//
-//static func getTrendingMovies(completionHandler: @escaping (_ result: Result<TrendingMoviesModel, NetworkError>) -> Void ) {
-//
-//    let urlString = NetworkConstant.shared.serverAddress + "trending/all/day?api_key=" + NetworkConstant.shared.apiKey
-//
-//    guard let url = URL(string: urlString) else {
-//        completionHandler(.failure(.urlError))
-//        return
-//    }
-//
-//    URLSession.shared.dataTask(with: url) { dataResponse, urlResponse, error in
-//
-//        if error == nil,
-//           let data = dataResponse,
-//           let resultData = try? JSONDecoder().decode(TrendingMoviesModel.self, from: data) {
-//            completionHandler(.success(resultData))
-//
-//        } else {
-//            completionHandler(.failure(.canNotParseData))
-//        }
-//    }.resume()
-//}
-
-//}
-
-
-
-
